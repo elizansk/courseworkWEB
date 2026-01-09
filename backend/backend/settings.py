2217220e278835be27f18c9fbea550866ec177ca
elizansk <liza.savkova1@gmail.com>
2026-01-09 20:57:52 +0700

    docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'KyrsovayaBD'),
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', '1234'),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),  # по умолчанию localhost
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DB_PORT', 5432),
     }
 }
